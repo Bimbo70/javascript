5f382ef81e5f1ed539a809b4ccca5979a03c15b8
Bimbo70 <barquerorojasdilan@gmail.com>
2026-08-26 10:53:27 -0600

    seccion 5

---
+++ seccion_5/2_declaracion if/misScripts2.js
@@ -0,0 +1,12 @@
+function evaluarCompra(cantidadDisponible){
+    let elementoRespuesta = document.
+    getElementById("decision");
+
+    let elementoCantidad = document.
+    getElementById("textoCantidad")
+    let cantidadComprada = elementoCantidad.value;
+
+    if (cantidadComprada <= cantidadDisponible) {
+        alert("Hola Mundo")
+    }
+    }
